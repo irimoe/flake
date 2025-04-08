098684782e06aeb9565a9798102f218e8da60afa
iris <iris@heyiris.dev>
2025-04-08 19:47:10 +0100

    feat: add zen derivation

--- src/packages/derivations/packages.nix
+++ src/packages/derivations/packages.nix
@@ -2,4 +2,6 @@
 {
   bun = pkgs.callPackage ./bun.nix { };
   zed-editor = pkgs.callPackage ./zed-editor.nix { };
+  zed-preview = pkgs.callPackage ./zed-editor-preview.nix { };
+  zen-browser = pkgs.callPackage ./zen-browser.nix { };
 }
