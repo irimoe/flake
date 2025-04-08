642e0fc4e02490d70363dcf48ded78e78e1b82cf
iris <iris@heyiris.dev>
2025-04-08 18:16:42 +0100

    feat: bump zed versions, add zed-preview

--- src/packages/derivations/zed-editor-preview.nix
+++ src/packages/derivations/zed-editor-preview.nix
@@ -18,12 +18,12 @@
 }:
 
 stdenv.mkDerivation rec {
-  pname = "zed";
-  version = "0.180.2";
+  pname = "zed-preview";
+  version = "0.181.4-pre";
 
   src = fetchurl {
     url = "https://github.com/zed-industries/zed/releases/download/v${version}/zed-linux-x86_64.tar.gz";
-    sha256 = "sha256-dWk2lleRnjLd/W8QiaAavpR5vRQKK94/LGt3CuYw2lI=";
+    sha256 = "sha256-G6Rktpws7knmpoxKwa2BlqxB8MdikiALSC/edyyqRtc=";
   };
 
   nativeBuildInputs = [
@@ -59,23 +59,22 @@ stdenv.mkDerivation rec {
     mkdir -p $out/share/applications
     mkdir -p $out/share/icons/hicolor/512x512/apps
 
-    tar -xvf $src
-    cp -r zed.app/* $out/lib/zed/
+    cp -r zed-preview.app/* $out/lib/zed/
 
     patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
         --set-rpath "${lib.makeLibraryPath buildInputs}" \
-        $out/lib/zed/bin/zed
+        $out/lib/zed/libexec/zed-editor
 
-    makeWrapper $out/lib/zed/bin/zed $out/bin/zeditor \
+    makeWrapper $out/lib/zed/libexec/zed-editor $out/bin/zeditor-preview \
         --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath buildInputs}" \
         --set WINIT_UNIX_BACKEND "x11" \
         --set LD_PRELOAD "${vulkan-loader}/lib/libvulkan.so.1"
 
-    cat > $out/share/applications/zed.desktop << EOF
+    cat > $out/share/applications/zed-preview.desktop << EOF
     [Desktop Entry]
-    Name=Zed Editor
+    Name=Zed Preview
     Comment=High-performance, multiplayer code editor
-    Exec=$out/bin/zeditor
+    Exec=$out/bin/zeditor-preview
     Icon=$out/lib/zed/share/icons/hicolor/512x512/apps/zed.png
     Terminal=false
     Type=Application
